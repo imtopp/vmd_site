80397d90dbe839f300ec57659a45b3acdfcce944
Taufiq Okta Pratama Putra <TOPPcomptech@gmail.com>
2016-12-22 17:44:24 +0700

    New Model Commit
    Aduh! lupa viewnya ketinggalan modelnya ^^v

---
+++ app/Models/ViewActiveProduct.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Category
+ * By: TOPP
+ */
+class ViewActiveProduct extends Model
+{
+    protected $table = 'view_active_product';
+
+    public $timestamps = false;
+
+    protected $fillable = [];
+
+    protected $guarded = [];
+
+
+}
